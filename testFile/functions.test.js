8d3e16be18bae3e32ab4cdc49946b93429654028
Averyguillermo <guillera19@up.edu>
2019-01-29 12:14:47 -0800

    Added a test that fails
    Per the sprint0 assignment, I added a test that fails.

--- testFile/functions.test.js
+++ testFile/functions.test.js
@@ -1,9 +1,16 @@
 const sum = require('../js/functions.js');
 
+//this test passes
 test('adds 3 + 4 to equal 7', () => {
      expect(sum(3, 4)).toBe(7);
      });
 
+//this test passes
 test('adds -3 + -4 to equal -7', () => {
      expect(sum(-3, -4)).toBe(-7);
+     });
+
+//this is the test that fails!!
+test('adds 3+ 3 to equal 11', () => {
+     expect(sum(3, 3)).toBe(11);
      });
