aafa88842c6bb652d229f4987dac205c9f0df4c4
Averyguillermo <guillera19@up.edu>
2019-01-29 17:53:56 -0800

    Added comments

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,10 @@
+//Avery Guillermo
+//functions.js
+
+//This contains the function that adds two numbers.
+
 function sum(x,y){
     return x+y;
 }
-module.exports = sum;
+module.exports = sum; //add this line for jest
 
--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
+//Avery Guillermo
+//index.js
+
+//This uses the sum function in functinos.js to write
+//information into the index.html
+
+//call the sum function
 var sum = sum(3,4);
+//modify index.html
 document.getElementById("header1").innerHTML = "The sum of 3 and 4 is: " + sum;
